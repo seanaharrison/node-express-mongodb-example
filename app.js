6594069bf43a9b01774542a3c8580f9fd182a7a7
Sarthak Khandelwal <sarthakgupta438@gmail.com>
2022-08-05 13:54:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var MongoClient = require('mongodb').MongoClient;
 var db;
 
 // setup mongo connection
-MongoClient.connect('mongodb://54.174.150.208:27017/myMongoDB', function(err, database) {
+MongoClient.connect('mongodb://127.0.0.1:27017/myMongoDB', function(err, database) {
 	if (err) {
 		throw err;
 	}
